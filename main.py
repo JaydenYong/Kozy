6c1f546c07ba276622b5100e8d639bc80aaa79ae
SecretSix <eejenyong@gmail.com>
2021-12-24 13:08:47 +0800

    added App Class. Atexit trigger for Delete File not working. Please fix.

---
+++ main.py
@@ -0,0 +1,27 @@
+import webbrowser as wb
+import os,atexit
+
+class App():
+    def __init__(self,filename="main"):
+        self.filename = filename + ".html"
+        self.writeHTML("<html>\n\t<body>\n\t\n</body>\n</html>")
+
+        atexit.register(self.delFile())
+    
+    def run(self):
+        wb.open(self.filename)
+        
+    def delFile(self):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+        else:
+            raise FileExistsError(f"{self.filename} does not exist.")
+    
+    def writeHTML(self,content):
+        with open(self.filename,"w") as html_file:
+            html_file.write(content)
+    
+    def Header(self,size,content):
+        pass
+
+my_app = App()
